ded4d2a1c4003d6391c4de5665913d564b3eccd4
B-raw <michaelbarry1989@hotmail.com>
2016-10-26 14:48:21 +0100

    Add all jquery + ajax functionality

---
+++ js/interface.js
@@ -0,0 +1,62 @@
+$(document).ready(function(){
+
+  var thermostat = new Thermostat();
+
+  function updateTemperature() {
+    $('#temperature').text(thermostat.temperature());
+    $('#temperature').attr('class', thermostat.displayUsage());
+  }
+
+//---INITIALISATION --- //
+  $('#temperature').text(thermostat.temperature());
+
+  $('#temperature').attr('class', thermostat.displayUsage());
+
+  displayWeather("London")
+//---END---//
+
+  $('#temp-up').on('click', function(){
+    thermostat.up();
+    updateTemperature();
+  })
+
+  $('#temp-down').on('click', function() {
+    thermostat.down();
+    updateTemperature();
+  })
+
+  $('#reset').on('click', function() {
+    thermostat.reset();
+    updateTemperature();
+  })
+
+  $('#powersaving-on').on('click', function() {
+    thermostat.savingPowerOn();
+    $('#power-saving-status').text("on");
+  })
+
+  $('#powersaving-off').on('click', function() {
+    thermostat.savingPowerOff();
+    $('#power-saving-status').text("off");
+  })
+
+  function displayWeather(city) {
+    var url = 'http://api.openweathermap.org/data/2.5/weather?q=' + city;
+    var token = '&appid=4abce62c7fe66553d73a13bfd9ebed0b';
+    var units = '&units=metric';
+    $.get(url + token + units, function(data){
+      $('#current-temperature').text(data.main.temp);
+    })
+  }
+
+  $('#current-city').change(function() {
+    var city = $('#current-city').val();
+    displayWeather(city)
+  })
+
+
+
+
+
+
+})
